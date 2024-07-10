17de321b74e9cd45cf0d8011151f5c88c500c0ff
aliwert <154356044+aliwert@users.noreply.github.com>
2024-07-10 16:59:18 +0300

    feat: get & post transactions

--- app.js
+++ app.js
@@ -18,4 +18,34 @@ const Todo = sequelize.define("todos", {
     type: DataTypes.STRING(256),
     allowNull: false,
   },
+  description: DataTypes.TEXT,
+  priority: {
+    type: DataTypes.TINYINT,
+    allowNull: false,
+    defaultValue: 0,
+  },
+  isDone: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+    defaultValue: false,
+  },
+});
+
+sequelize
+  .authenticate()
+  .then(() => console.log("succesfull"))
+  .catch(() => console.log("failed"));
+
+const router = express.Router();
+
+router.get("/todo", async (req, res) => {
+  const data = await Todo.findAll();
+  res.status(200).send({
+    error: false,
+    result: data,
+  });
+});
+
+router.post("/todo", async (req, res) => {
+  const data = await Todo.create(req.body);
 });
